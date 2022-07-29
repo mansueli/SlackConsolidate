9d5c5c7c1b85b743d0df5c297299f53257a6040a
Rodrigo Mansueli Nunes <rodrigo@mansueli.com>
2022-07-29 13:37:46 -0300

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE slack_channels (
     id SERIAL PRIMARY KEY,
     channel text,
     channel_id text,
-    p_level text,
+    p_level text DEFAULT ''::text NOT NULL,
     dest_channel text,
     dest_channel_id text,
     private int DEFAULT '0'::int NOT NULL
